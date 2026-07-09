c8695cedc1d358effac31b328ae659fa7da33a75
Cole1104 <kimduyoung3875@gmail.com>
2026-07-09 23:16:28 +0900

    delete hive motherfucker

--- lib/hive/hive_adapters.dart
+++
@@ -1,8 +0,0 @@
-import 'package:hive_ce/hive.dart';
-
-import '../models/task.dart';
-
-part 'hive_adapters.g.dart';
-
-@GenerateAdapters([AdapterSpec<Task>()], firstTypeId: 0)
-class HiveAdapters {}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
 import 'theme/app_theme.dart';
 import 'services/task_service.dart';
-import 'package:hive_ce_flutter/adapters.dart';
-import 'hive/hive_registrar.g.dart';
+
+import 'models/task.dart';
 
 final todayTaskService = TaskService();
 final recurringTaskService = TaskService(storageKey: "recurring_tasks");
@@ -11,12 +11,11 @@ final recurringTaskService = TaskService(storageKey: "recurring_tasks");
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Hive.initFlutter();
-  Hive.registerAdapters();
-
   await todayTaskService.init();
+
   await recurringTaskService.init();
   await recurringTaskService.resetDailyRoutineIfNeeded();
+
   runApp(const DandandoApp());
 }
 
